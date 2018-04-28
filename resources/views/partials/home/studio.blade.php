492b861dbf018fdb9f91fa643955660f7bbc659c
tapos <tapos.aa@gmail.com>
2018-04-28 23:30:56 +0600

    akash code some modified

--- resources/views/partials/home/studio.blade.php
+++ resources/views/partials/home/studio.blade.php
@@ -11,24 +11,27 @@
                     </div>
 
                     <div class="studio__left-top__inner-content"><!-- Inner should be hide/show on hover on plus button -->
-                        <div class="studio__left-top__inner-content__heading">
-                            <h2>Design graphique &nbsp;</h2>
-                            <h3>Imaginer des concepts</h3>
+                        <div class="innerText">
+                            <div class="studio__left-top__inner-content__heading">
+                                <h2>Design graphique &nbsp;</h2>
+                                <h3>Imaginer des concepts</h3>
+                            </div>
+
+                            <div class="studio__left-top__inner-content__details">
+                                <p class="paragraph u-justify-text">
+                                    <span>Service</span>
+                                </p>
+                                <ul>
+                                    <li>Direction artistique / Graphisme web et print</li>
+                                    <li>Conception logo / identité visuelle / Charte graphique</li>
+                                    <li>Webdesign / Responsive, UI et UX webdesign</li>
+                                    <li>Création tous types de supports imprimés</li>
+                                    <li>Photographie / Video / Motion design</li>
+                                </ul>
+                                <a href="#" class="btn-text">Learn more &rarr;</a>
+                            </div>
                         </div>
 
-                        <div class="studio__left-top__inner-content__details">
-                            <p class="paragraph u-justify-text">
-                                <span>Service</span>
-                            </p>
-                            <ul>
-                                <li>Direction artistique / Graphisme web et print</li>
-                                <li>Conception logo / identité visuelle / Charte graphique</li>
-                                <li>Webdesign / Responsive, UI et UX webdesign</li>
-                                <li>Création tous types de supports imprimés</li>
-                                <li>Photographie / Video / Motion design</li>
-                            </ul>
-                            <a href="#" class="btn-text">Learn more &rarr;</a>
-                        </div>
                     </div><!-- Inner End -->
 
                 </div>
@@ -36,19 +39,22 @@
 
             <div class="col-lg-5">
                 <div class="studio__right-top">
-                    <div class="studio__right-top__heading">
-                        <h2>Studio de creation &nbsp;</h2>
-                        <h3>Design graphique, Webdesign & Photographie/Video</h3>
+                    <div class="rightshowDiv">
+                        <div class="studio__right-top__heading">
+                            <h2>Studio de creation &nbsp;</h2>
+                            <h3>Design graphique, Webdesign & Photographie/Video</h3>
+                        </div>
+
+                        <div class="studio__right-top__details">
+                            <p class="paragraph u-justify-text">
+                                Nous imaginons votre communication visuelle afin de promouvoir votre activite, vos produits, informer vos clients et prospects, et vous demarquer de la concurrence.
+                                <br>
+                                Un etat d'esprit carre, une creativite sans cesse renouvelee qui vient jouer et briser les cadres etablis, une constante : toujours une pointe de fun !
+                            </p>
+                            <a href="#" class="btn-text">Learn more &rarr;</a>
+                        </div>
                     </div>
 
-                    <div class="studio__right-top__details">
-                        <p class="paragraph u-justify-text">
-                            Nous imaginons votre communication visuelle afin de promouvoir votre activite, vos produits, informer vos clients et prospects, et vous demarquer de la concurrence.
-                            <br>
-                            Un etat d'esprit carre, une creativite sans cesse renouvelee qui vient jouer et briser les cadres etablis, une constante : toujours une pointe de fun !
-                        </p>
-                        <a href="#" class="btn-text">Learn more &rarr;</a>
-                    </div>
 
                     <div class="studio__right-top__inner-content"><!-- Inner should be hide/show on hover on plus button -->
                         <div class="studio__right-top__inner-content__heading">
